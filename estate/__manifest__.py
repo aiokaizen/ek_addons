93dbcf0dabea88f29b1396d5aef0535dfaa0851b
Mouad Kommir <mouadkommir@gmail.com>
2024-03-01 00:29:20 +0100

    + Fix issues with Estate installation + Add master and demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -13,13 +13,21 @@
         'base',
     ],
     'data': [
+
         'security/ir.model.access.csv',
+
+        'data/master_data.xml',
+
         'views/estate_property_views.xml',
-        'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
+        'views/estate_property_type_views.xml',
         'views/res_users_views.xml',
+
         'menus/estate_menus.xml',
+    ],
+    'demo': [
+        'demo/demo_data.xml',
     ],
     'license': 'LGPL-3',
     'application': True,
