79b879a61a2727de50e2df0cb0c9501430a2071f
Mouad Kommir <mouadkommir@gmail.com>
2024-05-09 14:50:22 +0100

    Add more endpoints to ChronoDiali handlers

--- is_cd_connector/controllers/api.py
+++ is_cd_connector/controllers/api.py
@@ -31,3 +31,55 @@ class CronoDialiAPI(Controller):
             "status": "success",
             "data": receiver.read()
         })
+
+    @route('/api/chronodiali/test', auth='public', type="http")
+    def test_handler(self):
+        request.update_env(su=True)
+        receiver = request.env['receiver'].create([{
+            "api_url": "/api/chronodiali/test",
+            "api_data": request.get_json_data(),
+            "api_data2": json.dumps(request.get_json_data(), indent=4),
+        }])
+        return request.make_json_response({
+            "status": "success",
+            "data": receiver.read()
+        })
+
+    @route('/api/chronodiali/returned', auth='public', type="http")
+    def returned_handler(self):
+        request.update_env(su=True)
+        receiver = request.env['receiver'].create([{
+            "api_url": "/api/chronodiali/returned",
+            "api_data": request.get_json_data(),
+            "api_data2": json.dumps(request.get_json_data(), indent=4),
+        }])
+        return request.make_json_response({
+            "status": "success",
+            "data": receiver.read()
+        })
+
+    @route('/api/chronodiali/canceled', auth='public', type="http")
+    def canceled_handler(self):
+        request.update_env(su=True)
+        receiver = request.env['receiver'].create([{
+            "api_url": "/api/chronodiali/canceled",
+            "api_data": request.get_json_data(),
+            "api_data2": json.dumps(request.get_json_data(), indent=4),
+        }])
+        return request.make_json_response({
+            "status": "success",
+            "data": receiver.read()
+        })
+
+    @route('/api/chronodiali/delivery_success', auth='public', type="http")
+    def delivery_success_handler(self):
+        request.update_env(su=True)
+        receiver = request.env['receiver'].create([{
+            "api_url": "/api/chronodiali/delivery_success",
+            "api_data": request.get_json_data(),
+            "api_data2": json.dumps(request.get_json_data(), indent=4),
+        }])
+        return request.make_json_response({
+            "status": "success",
+            "data": receiver.read()
+        })
