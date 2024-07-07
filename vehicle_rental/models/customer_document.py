48ae9c602b5b81b97e5ca3273aadba2426f14f49
ahmed <k.ahmed@ekblocks.com>
2024-07-07 17:02:55 +0100

    handle task #0024

--- vehicle_rental/models/customer_document.py
+++ vehicle_rental/models/customer_document.py
@@ -14,6 +14,9 @@ class CustomerDocument(models.Model):
     vehicle_id = fields.Many2one(related='vehicle_contract_id.vehicle_id', string="Vehicle")
     file_name = fields.Char(string="File Name")
     avatar = fields.Binary(string="Document", required=True)
+    attachment_ids = fields.Many2many('ir.attachment', string='Attachments')
+    delivery_date = fields.Date(string="Délivré (e) le")
+    delivery_place = fields.Char(string="Délivré (e) à")
     document_type = fields.Selection([('dl', "Driving license"), ('passport', "Passport"),
                                       ('aadhaar_card', "Aadhaar card"), ('voter_id', "Voter ID card"),
                                       ('ration_card', "Ration card"), ('photo_id', "Photo ID card")],
