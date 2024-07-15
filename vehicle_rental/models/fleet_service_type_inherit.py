ea9191e22574885e390fae943a1657894fd3e5ab
ahmed <k.ahmed@ekblocks.com>
2024-07-15 11:34:48 +0100

    handle task #0033

--- vehicle_rental/models/fleet_service_type_inherit.py
+++ vehicle_rental/models/fleet_service_type_inherit.py
@@ -6,9 +6,4 @@ class FleetServiceType(models.Model):
 
     _inherit = 'fleet.service.type'
 
-    slug = fields.Char(string="Slug", compute="_compute_slug", store=True)
-    
-    @api.depends('name')
-    def _compute_slug(self):
-        for record in self:
-            record.slug = slugify(record.name)
+    slug = fields.Char(string="Slug")
