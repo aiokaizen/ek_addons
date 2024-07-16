1c4a99bb020352cc44f916ddb7be00365b4a2c65
ahmed <k.ahmed@ekblocks.com>
2024-07-16 15:33:19 +0100

    handle task #0038

--- vehicle_rental/models/type.py
+++ vehicle_rental/models/type.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from odoo import models, fields, api, _
 from odoo.addons.http_routing.models.ir_http import slugify
-
+from odoo.exceptions import UserError
 
 class Type(models.Model):
     """Type Paper"""
@@ -13,9 +13,19 @@ class Type(models.Model):
     sequence = fields.Integer(
         'Sequence', default=1, help=_("Utilisé pour commander des types. Le bas est le premier à apparaître.")
     )
+    auto_generated = fields.Boolean(default=False)
     slug = fields.Char(string="Slug", compute="_compute_slug", store=True)
 
     @api.depends('name')
     def _compute_slug(self):
         for record in self:
             record.slug = slugify(record.name)
+
+
+    def unlink(self):
+        for record in self:
+            # Perform any pre-deletion checks or actions
+            if record.auto_generated:
+                raise UserError("System-generated papers cannot be deleted.")
+        res = super().unlink()
+        return res
