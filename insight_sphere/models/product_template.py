f8d1dc1d29e04fbe6b2200d8b510cd71cc876719
ahmed <k.ahmed@ekblocks.com>
2024-07-23 15:16:35 +0100

    handle task #IS0001

--- insight_sphere/models/product_template.py
+++ insight_sphere/models/product_template.py
@@ -1,4 +1,6 @@
 from odoo import fields, models, api, _
+import random
+import string
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -13,3 +15,17 @@ class ProductTemplate(models.Model):
             _logger = logging.getLogger(__name__)
             _logger.warning("i am in hide invoice policy")
             record.hide_invoice_policy = False 
+        
+    def generate_random_barcode(self):
+        for product in self:
+            if not product.barcode:
+                product.barcode = ''.join(random.choices(string.digits, k=13))
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    def generate_random_barcode(self):
+        for product in self:
+            if not product.barcode:
+                product.barcode = ''.join(random.choices(string.digits, k=13))
