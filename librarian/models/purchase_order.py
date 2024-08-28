4217a3208224aa92821b6eab8fd434dc28415a72
Mouad Kommir <mouadkommir@gmail.com>
2024-08-28 13:43:19 +0100

    Update librarian consignment in process

--- librarian/models/purchase_order.py
+++ librarian/models/purchase_order.py
@@ -3,14 +3,16 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    is_consignment_in = fields.Boolean(string="Consignement")
-    has_receipt = fields.Boolean(
+    is_consignment_in = fields.Boolean(string="Dépôt Entré")
+
+    # @TODO: Should be removed if not used!
+    is_received = fields.Boolean(
         string='Has Receipt',
-        compute='_compute_has_receipt'
+        compute='_compute_is_received'
     )
 
     @api.depends('name')
-    def _compute_has_receipt(self):
+    def _compute_is_received(self):
         for order in self:
             # Search for related stock pickings (receipts)
             receipts = self.env['stock.picking'].search([
@@ -18,4 +20,4 @@ class PurchaseOrder(models.Model):
                 ('picking_type_id.code', '=', 'incoming'),
                 ('state', '!=', 'cancel')  # Optional: Exclude canceled receipts
             ])
-            order.has_receipt = bool(receipts)
+            order.is_received = bool(receipts)
--- librarian/models/purchase_order_line.py
+++ librarian/models/purchase_order_line.py
@@ -1,54 +1,66 @@
 from odoo import api, fields, models, _
 
 class PurchaseOrderLine(models.Model):
+
     _inherit = 'purchase.order.line'
 
-    counted_qte = fields.Float(string='Quantité comptabilisé', compute='_compute_counted_qte')
-
+    counted_qte = fields.Float(string='Quantité restant', compute='_compute_counted_qte')
 
     @api.depends()
     def _compute_counted_qte(self):
-        for rec in self:
-            # Get the related stock picking (receipt movement) for the purchase order line
-            receipt = self.env['stock.picking'].search([
-                ('purchase_id', '=', rec.order_id.id),
-                ('purchase_id.is_consignment_in', '=', True),
-                ('picking_type_id.code', '=', 'incoming')
-            ], order='scheduled_date asc', limit=1)
 
-            if receipt:
-                receipt_date = receipt.scheduled_date
+        current_order = self.order_id
+        print("current order:", current_order)
 
-                # Get valid sales that were created after the receipt movement
-                sales_after_movement = self.env['sale.order'].search([
-                    ('state', '=', 'sale'),  # Only consider confirmed sales
-                    ('date_order', '>', receipt_date)
-                ])
-                # Calculate total quantity sold after receipt
-                total_qty_sold = sum(
-                    sale_line.product_uom_qty
-                    for sale in sales_after_movement
-                    for sale_line in sale.order_line
-                    if sale_line.product_id == rec.product_id
-                )
+        all_orders = self.env["purchase.order"].search([
+            ("is_received", "=", True),
+            ("is_consignment_in", "=", True),
+            ("date_order", ">=", current_order.date_order),
+        ])
 
+        for order in all_orders:
 
-                # Find all purchase invoices for this product after the receipt date
-                subsequent_invoices = self.env['account.move.line'].search([
-                    ('product_id', '=', rec.product_id.id),
-                    ('move_id.move_type', '=', 'in_invoice'),  # Only consider purchase invoices
-                    ('move_id.state', '=', 'posted'),  # Only posted invoices
-                    ('move_id.invoice_date', '>=', receipt_date)  # Invoices after the receipt date
-                ])
-                # Calculate the total quantity invoiced after the receipt date
-                invoiced_qty_after_receipt = sum(line.quantity for line in subsequent_invoices)
-                print('--------------------------------------------------')
-                for line in subsequent_invoices:
-                    print('subsequent_invoices ', line.quantity , ' invoice id : ', line.move_id.name)
-                print('--------------------------------------------------')
-                
+            for rec in order.order_line:
+                # Get the related stock picking (receipt movement) for the purchase order line
+                receipt = self.env['stock.picking'].search([
+                    ('purchase_id', '=', rec.order_id.id),
+                    ('purchase_id.is_consignment_in', '=', True),
+                    ('picking_type_id.code', '=', 'incoming')
+                ], order='scheduled_date asc', limit=1)
 
-                # Subtract the invoiced quantity from total quantity sold
-                rec.counted_qte = total_qty_sold - invoiced_qty_after_receipt
-            else:
-                rec.counted_qte = 0.0
+                if receipt:
+                    receipt_date = receipt.scheduled_date
+
+                    # Get valid sales that were created after the receipt movement
+                    sales_after_movement = self.env['sale.order'].search([
+                        ('state', '=', 'sale'),  # Only consider confirmed sales
+                        ('date_order', '>', receipt_date)
+                    ])
+
+                    # Calculate total quantity sold after receipt
+                    total_qty_sold = sum(
+                        sale_line.product_uom_qty
+                        for sale in sales_after_movement
+                        for sale_line in sale.order_line
+                        if sale_line.product_id == rec.product_id
+                    )
+
+                    # Find all purchase invoices for this product after the receipt date
+                    subsequent_invoices = self.env['account.move.line'].search([
+                        ('product_id', '=', rec.product_id.id),
+                        ('move_id.move_type', '=', 'in_invoice'),  # Only consider purchase invoices
+                        ('move_id.state', '=', 'posted'),  # Only posted invoices
+                        ('move_id.invoice_date', '>=', receipt_date)  # Invoices after the receipt date
+                    ])
+
+                    # Calculate the total quantity invoiced after the receipt date
+                    print("loop order:", rec.order_id)
+                    invoiced_qty_after_receipt = sum(line.quantity for line in subsequent_invoices)
+                    if rec.order_id == current_order:
+                        # Subtract the invoiced quantity from total quantity sold
+                        rec.counted_qte = total_qty_sold - invoiced_qty_after_receipt
+                    else:
+                        counted_qte = total_qty_sold - invoiced_qty_after_receipt
+                        rec.counted_qte = counted_qte if counted_qte >= 0 else 0
+                else:
+                    rec.counted_qte = 0.0
