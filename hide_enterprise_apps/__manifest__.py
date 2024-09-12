3e7a1101e879df26f5576ff090ddff1d1f1b52f8
ahmed <k.ahmed@ekblocks.com>
2024-09-12 16:22:05 +0100

    add app hide enterprise apps from list of apps

---
+++ hide_enterprise_apps/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Hide enterprise apps Module
+# Author: EKBlocks
+{
+    'name': 'Hide Enterprise apps',
+    'version': '2.0',
+    'author': 'EKBlocks',
+    'category': 'Sales',
+    'summary': 'Hide enterprise apps.',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/hide_enterprise_apps.xml',
+    ],
+    'demo': [],
+    'license': 'Other proprietary',
+    'application': True,
+}
